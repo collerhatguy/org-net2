0ac7b23d8300ae1d8bbe6cf32973a5e8833b6cde
Jacob Shomali <jshomali@nexient.com>
2022-08-16 09:47:57 -0500

    added working validation

--- Controllers/EmployessController.cs
+++ Controllers/EmployessController.cs
@@ -14,11 +14,19 @@ namespace OrgChartApi.Controllers {
     public ActionResult<List<Employee>> GetEmployees() {
       return Ok(context.Employees.ToList());
     }
+    // public EmployeeDto PostEmployee([FromBody] EmployeeDto body) {
     [HttpPost]
     public async Task<ActionResult<List<Employee>>> PostEmployee([FromBody] EmployeeDto body) {
+      if (body is null) {
+        return NotFound("bad request");
+      }
       var job = await context.Jobs.FindAsync(body.jobId);
       if (job is null) {
-        return NotFound();
+        return NotFound("Job does not exist");
+      }
+      var department = await context.Departments.FindAsync(body.departmentId);
+      if (department is null) {
+        return NotFound("Department does not exist");
       }
       return GetEmployees();
     }
